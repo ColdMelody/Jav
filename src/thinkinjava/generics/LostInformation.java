c1e578b411737cd97648935ea3ff90ac17bc9f2f
ColdMelody <androidBaoCP@163.com>
2016-06-28 23:54:23 +0800

    在泛型代码内部，无法获得任何有关泛型参数类型的信息

---
+++ src/thinkinjava/generics/LostInformation.java
@@ -0,0 +1,24 @@
+package thinkinjava.generics;
+
+import java.util.*;
+
+/**
+ * Created by Bill on 2016/6/28.
+ * Email androidBaoCP@163.com
+ */
+public class LostInformation {
+    public static void main(String[] args){
+        List<Forb> lists=new ArrayList<>();
+        Map<Forb,Fnorkel> map=new HashMap<>();
+        Quark<Fnorkel> quark=new Quark<>();
+        Particle<Long,Double> particle=new Particle<>();
+        System.out.println(Arrays.asList(lists.getClass().getTypeParameters()));
+        System.out.println(Arrays.asList(map.getClass().getTypeParameters()));
+        System.out.println(Arrays.asList(quark.getClass().getTypeParameters()));
+        System.out.println(Arrays.asList(particle.getClass().getTypeParameters()));
+    }
+}
+class Forb{}
+class Fnorkel{}
+class Quark<Q>{}
+class Particle<POSITION,MOMENTUM>{}
